09472a6560a63b11b5b1dbc0a43885c6ff1fb4d5
pylover <vahid.mardani@gmail.com>
2019-01-21 00:19:25 +0330

    mDNS to easily discover in lan

--- firmware/user_main.c
+++ firmware/user_main.c
@@ -20,15 +20,32 @@ tick_cb(uint32_t ticks) {
 }
 
 
+static void ICACHE_FLASH_ATTR
+setup_mdns() {
+	wifi_set_broadcast_if (STATIONAP_MODE);
+	struct ip_info ipconfig;
+	wifi_get_ip_info(STATION_IF, &ipconfig);
+	
+	struct mdns_info *info = \
+				(struct mdns_info *)os_zalloc(sizeof(struct mdns_info));
+	info->host_name = DEVICE_NAME;
+	info->ipAddr= ipconfig.ip.addr; //sation ip
+	info->server_name = DEVICE_NAME"Server";
+	info->server_port = 80;
+	info->txt_data[0] = "version = 1.0.1";
+	espconn_mdns_init(info);
+}
+
+
 static void ICACHE_FLASH_ATTR 
 wifi_connect_cb(uint8_t status) {
     if(status == STATION_GOT_IP) {
 		os_printf("Wifi connected\r\n");
+		setup_mdns();
     } else {
 		os_printf("Wifi disconnected\r\n");
     }
 }
-
 
 
 void ICACHE_FLASH_ATTR 
