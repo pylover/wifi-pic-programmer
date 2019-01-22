a9fb28c9f4f27b2dd4d0163d31afb953782794cd
pylover <vahid.mardani@gmail.com>
2019-01-22 22:35:47 +0330

    Show version, -V/--version

--- picfota/easycli.py
+++ picfota/easycli.py
@@ -59,12 +59,18 @@ class Root(Command):
 
     def __init__(self, argv=None):
         super().__init__()
-        args = self._parser.parse_args(argv)
+        self.launch(argv)
 
-        if hasattr(args, 'func'):
-            exitcode = args.func(args)
-        else:
-            exitcode = self(args)
+    def launch(self, argv):
+        args = self._parser.parse_args(argv)
+        try:
+            if hasattr(args, 'func'):
+                exitcode = args.func(args)
+            else:
+                exitcode = self(args)
+        except:
+            traceback.print_exc()
+            exitcode = 1
 
         sys.exit(exitcode or 0)
 
--- picfota/root.py
+++ picfota/root.py
@@ -4,10 +4,19 @@ from .easycli import Root, Argument
 
 
 class PicFota(Root):
-    
+    __help__ = 'WIFI PIC Programmer'
+
     __arguments__ = [
-        Argument('-v', '--v', action='store_true', help='Show version')
+        Argument('-V', '--version', action='store_true', help='Show version')
     ]
+
+    def __call__(self, args):
+        if args.version:
+            import picfota
+            print(picfota.__version__)
+            return
+
+        return super().__call__(args)
 
 #    __subcommands__ = [
 #        Firmware,
